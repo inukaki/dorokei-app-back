0e8f2fe97fbd1fa9f943fc258d98998a5eb4a853
inukaki <inukaki8@gmail.com>
2025-11-10 15:42:17 +0900

    createGameModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoomsModule } from './modules/rooms/rooms.module';
 import { PlayersModule } from './modules/players/players.module';
 import { typeOrmConfig } from 'typeorm.config';
+import { GameModule } from './modules/game/game.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { typeOrmConfig } from 'typeorm.config';
     TypeOrmModule.forRoot(typeOrmConfig),
     RoomsModule,
     PlayersModule,
+    GameModule,
   ],
   controllers: [],
   providers: [],
